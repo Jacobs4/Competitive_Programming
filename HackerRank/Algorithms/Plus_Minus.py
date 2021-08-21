5ea7209cd08d329e7c734a3206c40ce90050e9d8
Anil Kumar <anilkumarpdkn@gmail.com>
2021-08-21 12:02:00 +0530

    Update Plus_Minus.py

--- HackerRank/Algorithms/Plus_Minus.py
+++ HackerRank/Algorithms/Plus_Minus.py
@@ -1,17 +1,4 @@
-#!/bin/python3
-
-import math
-import os
-import random
-import re
-import sys
-
-#
-# Complete the 'plusMinus' function below.
-#
-# The function accepts INTEGER_ARRAY arr as parameter.
-#
-
+# replace your function with this
 def plusMinus(arr):
     n = len(arr)
     positive =  0
@@ -34,11 +21,3 @@ def plusMinus(arr):
         print('%.6f' % (negative/n))
         print('%.6f' % (zero/n))
         return
-     
-
-if __name__ == '__main__':
-    n = int(input().strip())
-
-    arr = list(map(int, input().rstrip().split()))
-
-    plusMinus(arr)
