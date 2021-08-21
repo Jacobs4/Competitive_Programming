070374e77ccdf38b44b1a1dd5b44b1e135b62125
Anil Kumar <anilkumarpdkn@gmail.com>
2021-08-21 12:03:08 +0530

    Update Solve_Me_First.py

--- HackerRank/Algorithms/Solve_Me_First.py
+++ HackerRank/Algorithms/Solve_Me_First.py
@@ -1,9 +1,3 @@
+# replace your function with this
 def solveMeFirst(a,b):
     return a+b
-	# Hint: Type return a+b below
-
-
-num1 = int(input())
-num2 = int(input())
-res = solveMeFirst(num1,num2)
-print(res)
