470cffa0ed16fd4ff25405cee092a2274b9f35d1
Anil Kumar <anilkumarpdkn@gmail.com>
2021-08-21 11:59:44 +0530

    Update A_Very_Big_Sum.py

--- HackerRank/Algorithms/A_Very_Big_Sum.py
+++ HackerRank/Algorithms/A_Very_Big_Sum.py
@@ -1,33 +1,8 @@
 #people who are not using python, this ain't gonna work for them, you need to change the datatypes may to long long ?
 
-import math
-import os
-import random
-import re
-import sys
-
-#
-# Complete the 'aVeryBigSum' function below.
-#
-# The function is expected to return a LONG_INTEGER.
-# The function accepts LONG_INTEGER_ARRAY ar as parameter.
-#
-
+#replace your function with this
 def aVeryBigSum(ar):
     ans = 0
     for  i in range(0, len(ar)):
         ans += ar[i]
     return ans
-
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
-    ar_count = int(input().strip())
-
-    ar = list(map(int, input().rstrip().split()))
-
-    result = aVeryBigSum(ar)
-
-    fptr.write(str(result) + '\n')
-
-    fptr.close()
