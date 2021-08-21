b36605905ece6fde7914041ff0ff1e56af8bc8a3
Anil Kumar <anilkumarpdkn@gmail.com>
2021-08-21 12:11:47 +0530

    Update Plus_Minus.py

--- HackerRank/Algorithms/Plus_Minus.py
+++ HackerRank/Algorithms/Plus_Minus.py
@@ -4,20 +4,14 @@ def plusMinus(arr):
     positive =  0
     negative = 0
     zero = 0
-    if n == 0:
-        print('0.000000')
-        print('0.000000')
-        print('0.000000')
-        return
-    else:
-        for i in range(0,n):
-            if arr[i] > 0:
-                positive += 1
-            elif arr[i] <  0:
-                negative += 1
-            else:
-                zero += 1
-        print('%.6f' % (positive/n))
-        print('%.6f' % (negative/n))
-        print('%.6f' % (zero/n))
-        return
+    for i in range(0,n):
+        if arr[i] > 0:
+            positive += 1
+        elif arr[i] <  0:
+            negative += 1
+        else:
+            zero += 1
+    print('%.6f' % (positive/n))
+    print('%.6f' % (negative/n))
+    print('%.6f' % (zero/n))
+    return
