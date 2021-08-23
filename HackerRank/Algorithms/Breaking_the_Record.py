070d5df8a99df9bb6197e3414b7cd334b079a118
Cherprang <anilkumarpdkn@gmail.com>
2021-08-23 18:09:31 +0530

    Update Breaking_the_Record.py

--- HackerRank/Algorithms/Breaking_the_Record.py
+++ HackerRank/Algorithms/Breaking_the_Record.py
@@ -1,1 +1,12 @@
-gg ez
+def breakingRecords(scores):
+    min = scores[0]
+    max = scores[0]
+    ans = [0, 0]
+    for i in range(1, len(scores)):
+        if scores[i] < min:
+            min = scores[i]
+            ans[1] += 1
+        if scores[i] > max:
+            max = scores[i]
+            ans[0] += 1
+    return ans
