57f269db90f2df1a7e7bad8b2157a98b5c16e675
Cherprang <anilkumarpdkn@gmail.com>
2021-08-26 14:08:57 +0530

    Update Counting_Valleys.py

--- HackerRank/Algorithms/Counting_Valleys.py
+++ HackerRank/Algorithms/Counting_Valleys.py
@@ -1,1 +1,14 @@
-
+def countingValleys(steps, path):
+    ans = 0
+    flag = False
+    temp  = 0
+    for i in path:
+        if i == "D":
+            flag = True
+            temp -= 1
+        else:
+            temp += 1
+        if flag == True and temp  == 0 and i == "U":
+            ans += 1
+            flag =False
+    return ans
