c77bb4db123a1380cfd5b095f0069c1ca68717b5
Cherprang <anilkumarpdkn@gmail.com>
2021-08-26 22:15:50 +0530

    Update Picking_Numbers.py

--- HackerRank/Algorithms/Picking_Numbers.py
+++ HackerRank/Algorithms/Picking_Numbers.py
@@ -1,1 +1,8 @@
-
+def pickingNumbers(a):
+    temp = [0]*100
+    ans = []
+    for i in a:
+        temp[i]+=1
+    for i in range(len(temp)-1):
+        ans.append(temp[i]+temp[i+1])
+    return max(ans)
