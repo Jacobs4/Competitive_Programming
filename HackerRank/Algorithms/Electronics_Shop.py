45db98a0ff14bc8a2ce5b604db76865326bcdede
Cherprang <anilkumarpdkn@gmail.com>
2021-08-26 14:20:36 +0530

    Update Electronics_Shop.py

--- HackerRank/Algorithms/Electronics_Shop.py
+++ HackerRank/Algorithms/Electronics_Shop.py
@@ -1,1 +1,14 @@
-
+def getMoneySpent(keyboards, drives, b):
+    keyboards.sort(reverse=True)
+    drives.sort(reverse=True)
+    temp1 = 0
+    ans = []
+    for i in keyboards:
+        for j in drives:
+            if i+j<=b:
+                ans.append(i+j)
+    if len(ans)==0:       
+        return -1
+    else:
+        ans.sort(reverse=True)
+        return ans[0]
