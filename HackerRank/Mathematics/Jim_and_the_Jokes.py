8fa45bb4cf2a74e082588ecb6aca55254c7f4c7c
Cherprang <anilkumarpdkn@gmail.com>
2021-08-30 23:31:45 +0530

    Update Jim_and_the_Jokes.py

--- HackerRank/Mathematics/Jim_and_the_Jokes.py
+++ HackerRank/Mathematics/Jim_and_the_Jokes.py
@@ -5,18 +5,16 @@ def dec(base, n):
             return -1
         ans = (int(i)+ans*base)
     return ans
-
-
 def solve(dates):
-    change = [0]*100
+    temp = [0]*100
     for i in range(len(dates)):
         base = int(dates[i][0])
         day = dates[i][1]
         tod = dec(int(base), day)
         if tod != -1:
-            change[tod] += 1
+            temp[tod] += 1
     ans = 0
-    change.sort(reverse=True)
-    for i in change:
+    temp.sort(reverse=True)
+    for i in temp:
         ans += (i*(i-1)//2)
     return ans
