48621820b1c06ce280c6a161d409e9eeaffe9b78
Cherprang <anilkumarpdkn@gmail.com>
2021-08-30 17:25:45 +0530

    Create Angry_Professor.py

---
+++ HackerRank/Algorithms/Angry_Professor.py
@@ -0,0 +1,10 @@
+def angryProfessor(k, a):
+    count = 0
+    a.sort()
+    for i in a:
+        if i<=0:
+            count += 1
+    if count >= k:
+        return "NO"
+    else:
+        return "YES"
